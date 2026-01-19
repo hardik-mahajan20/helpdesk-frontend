485875131d5c6d66c0aaf2f1e4d01cb07b6e6060
Hardik Mahajan <hardikmahajan2010@gmail.com>
2026-01-19 17:33:20 +0530

    feat :- profile update feature completed

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,23 +5,90 @@ import {
   Tab,
   Typography,
   Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow
+  Card,
+  CardContent,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  Tooltip,
+  IconButton,
+  Button
 } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person'
 import LockIcon from '@mui/icons-material/Lock'
 import SettingsIcon from '@mui/icons-material/Settings'
 import './Profile.scss'
 import SecurityTab from './SecurityTab'
+import { useProfileSelectionStore } from '../../services/profile-selection-service'
+import type { UserProfileResponse } from '../../interfaces/profile'
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
+import { updateProfile } from '../../services/profile-service'
 
 function TabPanel ({ value, index, children }: any) {
   return value === index ? <Box sx={{ mt: 3 }}>{children}</Box> : null
 }
+type ProfileForm = {
+  firstName: string
+  lastName: string
+  email: string
+  phoneNumber: string
+  reportsToName: string
+  reportsToEmail: string
+}
 
 export default function Profile () {
   const [tabIndex, setTabIndex] = useState(0)
+
+  const { getProfile } = useProfileSelectionStore()
+
+  const [profileData, setProfileData] = useState<UserProfileResponse | null>(
+    null
+  )
+  const [profileForm, setProfileForm] = useState<ProfileForm>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    reportsToName: '',
+    reportsToEmail: ''
+  })
+
+  useState(() => {
+    const loadProfile = async () => {
+      const profile = await getProfile()
+      if (!profile) return
+      setProfileData(profile)
+      setProfileForm({
+        firstName: profile.firstName ?? '',
+        lastName: profile.lastName ?? '',
+        email: profile.email ?? '',
+        phoneNumber: profile.phoneNumber ?? '',
+        reportsToName: profile.reportsToPersonName ?? '',
+        reportsToEmail: profile.reportsToPersonEmail ?? ''
+      })
+    }
+    loadProfile()
+  })
+
+  const saveUserData = async (): Promise<void> => {
+    const payload = {
+      firstName: profileForm.firstName,
+      lastName: profileForm.lastName,
+      email: profileForm.email,
+      phoneNumber: profileForm.phoneNumber
+    }
+    await updateProfile(payload)
+  }
+
+  const handleChange =
+    (field: keyof ProfileForm) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setProfileForm(prev => ({
+        ...prev,
+        [field]: event.target.value
+      }))
+    }
 
   return (
     <div className='profile-container h-100 p-2 p-lg-3'>
@@ -49,26 +116,218 @@ export default function Profile () {
 
         <TabPanel value={tabIndex} index={0}>
           <Paper variant='outlined'>
-            <Table>
-              <TableBody>
-                <TableRow>
-                  <TableCell width='30%'>Full Name</TableCell>
-                  <TableCell>Harsh Kumar</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Email</TableCell>
-                  <TableCell>harsh@example.com</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Role</TableCell>
-                  <TableCell>Administrator</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Status</TableCell>
-                  <TableCell>Active</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+            <div className='tab-content py-3 py-sm-4 px-0'>
+              <Card className='section-card'>
+                <CardContent>
+                  <div className='profile-info flex-wrap d-flex flex-column flex-lg-row align-items-lg-start gap-4'>
+                    <div className='avatar-section d-flex flex-column align-items-center flex-sm-row align-items-sm-start gap-3'>
+                      <div className='avatar-container d-flex flex-column align-items-center position-relative'>
+                        <div className='avatar-wrapper position-relative'>
+                          {/* @if(!imageLoadFailed && (previewImageUrl || profileData()?.avatarUrl)) {
+              <img
+                [src]="previewImageUrl || profileData()?.avatarUrl"
+                alt="Profile Avatar"
+                className="avatar-img"
+                (error)="onImageError()"
+              />
+              } @else { */}
+                          <div className='avatar-initials w-100 h-100 d-flex justify-content-center align-items-center'>
+                            HM
+                          </div>
+                          {/* } */}
+                        </div>
+                        <input
+                          type='file'
+                          id='avatarInput'
+                          accept='image/*'
+                          hidden
+                        />
+                        <label className='camera-btn d-flex justify-content-center align-items-center text-white position-absolute'>
+                          <PhotoCameraIcon>
+                          </PhotoCameraIcon>
+                        </label>
+                      </div>
+                      <div className='user-info text-center text-sm-start mt-3 mt-sm-0 ms-sm-3'>
+                        <h3>
+                          {profileData?.firstName} {profileData?.lastName}
+                        </h3>
+                        <p className='user-email'>{profileData?.email}</p>
+                        <span className='user-role'>
+                          {profileData?.roleName}
+                        </span>
+                        {profileData?.roleName && (
+                          <span className='user-role ms-2'>
+                            {profileData?.department}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <div className='form-section flex-grow-1'>
+                      <div className='form-row mb-3 gap-3 align-items-start justify-content-between'>
+                        <FormControl variant='outlined' className='mb-1 flex-1'>
+                          <InputLabel
+                            htmlFor='outlined-adornment-first-name'
+                            className='mono-font'
+                          >
+                            First Name
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-first-name'
+                            type='text'
+                            required
+                            value={profileForm.firstName}
+                            onChange={handleChange('firstName')}
+                            label='First Name'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User First Name'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+
+                        <FormControl variant='outlined' className='mb-1 '>
+                          <InputLabel
+                            htmlFor='outlined-adornment-last-name'
+                            className='mono-font'
+                          >
+                            Last Name
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-last-name'
+                            type='text'
+                            required
+                            value={profileForm.lastName}
+                            onChange={handleChange('lastName')}
+                            label='Last Name'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User Last Name'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </div>
+                      <div className='form-row mb-3 gap-3 align-items-start justify-content-between'>
+                        <FormControl variant='outlined' className='mb-1'>
+                          <InputLabel
+                            htmlFor='outlined-adornment-email'
+                            className='mono-font'
+                          >
+                            Email
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-email'
+                            type='text'
+                            required
+                            value={profileForm.email}
+                            onChange={handleChange('email')}
+                            label='Email'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User Email'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+
+                        <FormControl variant='outlined' className='mb-1'>
+                          <InputLabel
+                            htmlFor='outlined-adornment-phone'
+                            className='mono-font'
+                          >
+                            Phone
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-phone'
+                            type='text'
+                            required
+                            value={profileForm.phoneNumber}
+                            onChange={handleChange('phoneNumber')}
+                            label='Phone'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User Phone Number'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </div>
+                      <div className='form-row mb-3 gap-3 align-items-start justify-content-between'>
+                        <FormControl variant='outlined' className='mb-1'>
+                          <InputLabel
+                            htmlFor='outlined-adornment-reports-to'
+                            className='mono-font'
+                          >
+                            Reports To (Name)
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-reports-to'
+                            type='text'
+                            readOnly
+                            value={profileForm.reportsToName}
+                            label='Reports To (Name)'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User Reports to name'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+
+                        <FormControl variant='outlined' className='mb-1'>
+                          <InputLabel
+                            htmlFor='outlined-adornment-reports-to-email'
+                            className='mono-font'
+                          >
+                            Reports To (Email)
+                          </InputLabel>
+
+                          <OutlinedInput
+                            id='outlined-adornment-reports-to-email'
+                            type='text'
+                            readOnly
+                            value={profileForm.reportsToEmail}
+                            label='Reports To (Email)'
+                            endAdornment={
+                              <InputAdornment position='end'>
+                                <Tooltip title='User Reports to email'>
+                                  <IconButton edge='end' />
+                                </Tooltip>
+                              </InputAdornment>
+                            }
+                          />
+                        </FormControl>
+                      </div>
+                      <div className='form-actions d-flex justify-content-end'>
+                        <Button
+                          variant='contained'
+                          color='primary'
+                          onClick={() => saveUserData()}
+                        >
+                          Save Changes
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
           </Paper>
         </TabPanel>
 
--- src/services/profile-service.ts
+++ src/services/profile-service.ts
@@ -1,7 +1,10 @@
+import { API_BASE_URL } from '../api'
 import { httpRequest } from '../api/http-Client'
 import { HTTP_METHOD } from '../enums'
+import type { ApiResponse } from '../interfaces'
 import type UpdateAgentRequest from '../interfaces/agent/update-agent-request'
 import type { UpdatePassword } from '../interfaces/profile'
+import { getToken } from './auth-service'
 
 const PROFILE_URL = 'profile'
 
@@ -52,4 +55,40 @@ export async function disableTwoFactorAuth<T> () {
 export async function verifyTwoFactorAuth<ApiResponse> (code: string) {
   const url = `${PROFILE_URL}/verify-2fa-setup`
   return httpRequest<ApiResponse>(url, HTTP_METHOD.POST, code)
+}
+
+export async function updateProfile<T> (payload: any) {
+  // This api endpoint need the payload in the FORM formate
+  const BASE_URL = API_BASE_URL
+  const url = `${PROFILE_URL}/profile-update`
+  const token = getToken()
+
+  const formData = new FormData()
+  Object.entries(payload).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      formData.append(key, value as any)
+    }
+  })
+
+  const response = await fetch(`${BASE_URL}/${url}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    body: formData
+  })
+
+  return handleResponse<T>(response)
+}
+
+// Helper Function
+async function handleResponse<T> (response: Response): Promise<T> {
+  const responseJson: ApiResponse<T> = await response.json()
+
+  if (!response.ok) {
+    const messages = responseJson.messages
+    const errorMessage = messages?.join(', ') ?? 'Request failed'
+    throw new Error(errorMessage)
+  }
+  return responseJson.data
 }
