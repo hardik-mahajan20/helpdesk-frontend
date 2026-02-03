580a208b94b13809f66116b29333e0980aebdebb
Hardik Mahajan <hardikmahajan2010@gmail.com>
2026-02-03 16:15:58 +0530

    fix :- authentication check on page reload and page change

--- src/guard/ProtectedRoute.tsx
+++ src/guard/ProtectedRoute.tsx
@@ -1,9 +1,38 @@
 import { Navigate, Outlet } from 'react-router-dom'
-import { isAuthenticated } from '../services/auth-service'
+import { useEffect, useState } from 'react'
+import { isTokenExpired, refreshToken, logout } from '../services/auth-service'
 
 export default function ProtectedRoute () {
-  if (!isAuthenticated()) {
-    return <Navigate to='/' replace />
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isAuth, setIsAuth] = useState<boolean>(false)
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      if (!isTokenExpired()) {
+        setIsAuth(true)
+        setLoading(false)
+        return
+      }
+
+      try {
+        await refreshToken()
+        setIsAuth(true)
+      } catch {
+        logout()
+        setIsAuth(false)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    checkAuth()
+  }, [])
+
+  if (loading) return null
+
+  if (!isAuth) {
+    return <Navigate to='/login' replace />
   }
+
   return <Outlet />
 }
