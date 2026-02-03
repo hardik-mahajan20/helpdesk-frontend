7c3dc7f9c4f14535d627077ee28833db01fe78af
Hardik Mahajan <hardikmahajan2010@gmail.com>
2026-02-03 16:08:27 +0530

    fix :- profile service modified

--- src/services/profile-service.ts
+++ src/services/profile-service.ts
@@ -1,18 +1,17 @@
-import { API_BASE_URL, httpRequestAsync } from '../api'
-// import { httpRequest } from '../api/http-Client'
-import { ColorOption, HTTP_METHOD, ThemeOption } from '../enums'
+import api from '../api/axios'
+import { ColorOption, ThemeOption } from '../enums'
 import type { ApiResponse } from '../interfaces'
 import type {
   EnableTwoFactorAuthResponse,
   UpdatePassword
 } from '../interfaces/profile'
-import { getToken } from './auth-service'
 
 const PROFILE_URL = 'profile'
 
 export async function changePassword<T> (payload: UpdatePassword) {
   const url = `${PROFILE_URL}/change-password`
-  return httpRequestAsync<T>(url, HTTP_METHOD.POST, payload)
+  const res = await api.post<ApiResponse<T>>(url, payload)
+  return res.data
 }
 
 export async function updateUserPreferences<T> (payload: {
@@ -22,29 +21,30 @@ export async function updateUserPreferences<T> (payload: {
   }
 }) {
   const url = `${PROFILE_URL}/update-preference-settings`
-  return httpRequestAsync<T>(url, HTTP_METHOD.POST, payload)
+  const res = await api.post<ApiResponse<T>>(url, payload)
+  return res.data
 }
 
 export async function enableTwoFactorAuth () {
   const url = `${PROFILE_URL}/enable-2fa`
-  return httpRequestAsync<EnableTwoFactorAuthResponse>(url, HTTP_METHOD.POST)
+  const res = await api.post<ApiResponse<EnableTwoFactorAuthResponse>>(url)
+  return res.data
 }
 
 export async function disableTwoFactorAuth<T> () {
   const url = `${PROFILE_URL}/disable-2fa`
-  return httpRequestAsync<T>(url, HTTP_METHOD.POST)
+  const res = await api.post<ApiResponse<T>>(url)
+  return res.data
 }
 
 export async function verifyTwoFactorAuth (code: string) {
   const url = `${PROFILE_URL}/verify-2fa-setup`
-  return httpRequestAsync<string[]>(url, HTTP_METHOD.POST, code)
+  const res = await api.post<ApiResponse<string[]>>(url, code)
+  return res.data
 }
 
 export async function updateProfile<T> (payload: any): Promise<ApiResponse<T>> {
-  // This api endpoint need the payload in the FORM formate
-  const BASE_URL = API_BASE_URL
   const url = `${PROFILE_URL}/profile-update`
-  const token = getToken()
 
   const formData = new FormData()
   Object.entries(payload).forEach(([key, value]) => {
@@ -53,25 +53,6 @@ export async function updateProfile<T> (payload: any): Promise<ApiResponse<T>> {
     }
   })
 
-  const response = await fetch(`${BASE_URL}/${url}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    body: formData
-  })
-
-  return handleResponse<T>(response)
-}
-
-// Helper Function
-async function handleResponse<T> (response: Response): Promise<ApiResponse<T>> {
-  const responseJson: ApiResponse<T> = await response.json()
-
-  if (!response.ok) {
-    const messages = responseJson.messages
-    const errorMessage = messages?.join(', ') ?? 'Request failed'
-    throw new Error(errorMessage)
-  }
-  return responseJson
+  const res = await api.post<ApiResponse<T>>(url, formData)
+  return res.data
 }
